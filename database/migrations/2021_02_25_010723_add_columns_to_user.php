43ea87ce31f89786750d9434e906d1193af82e77
JSween <61639420+JSween-Code@users.noreply.github.com>
2021-03-11 14:37:22 +0000

    Add files via upload

---
+++ database/migrations/2021_02_25_010723_add_columns_to_user.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->unique();
+            $table->string('org_id')->unique();
+            $table->boolean('isVendor');
+            $table->boolean('isAdmin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
