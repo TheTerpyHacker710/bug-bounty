0fd31e19c4e523ecd7c9c6ccfceb6a405fbfeace
Euan Garden <1802104@uad.ac.uk>
2021-03-23 10:20:01 -0700

    Admin Show reports and verifications

---
+++ resources/js/Pages/Admin/AdminActiveJobs.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="text-center px-4 py-4">
+        <h2>Active Jobs</h2>
+    </div>
+
+    <div class="grid grid-cols-2 gap-4 px-1 py-1 h-auto">
+        <span class="px-4 py-4 border-4 ">
+                <h2 class="text-center py-2">Reports to Verify</h2>
+                <ul v-if="activeVerifications.length > 0">
+                    <div v-for="verification in activeVerifications" :key="verification.id" class="py-2">
+                        <active-verifications :verification="verification" />
+                    </div>
+                </ul>
+
+                <ul v-else>
+                    <li class="text-center py-4 italic font-thin">No Active Verifications</li>
+                </ul>
+        </span>
+
+        <span class="px-4 py-4 border-4">
+                <h2 class="text-center py-2">Active Reports</h2>
+                <ul v-if="activeReports.length > 0">
+                    <div :key="report.id" v-for="report in activeReports" class="py-2">
+                        <active-report :report="report"/>
+                    </div>
+                </ul>
+
+                <ul v-else>
+                    <li class="text-center py-4 italic font-thin">No Active Reports</li>
+                </ul>
+        </span>
+    </div>
+
+</template>
+
+<script>
+
+import ActiveReport from '../Dashboard/ActiveReport.vue'
+import ActiveVerifications from '../Dashboard/ActiveVerifications.vue'
+
+export default {
+    props: ['activeReports', 'activeVerifications'],
+
+    components: {
+        ActiveReport,
+        ActiveVerifications
+    },
+}
+
+</script>
