6e4667c7d76fea1ffbdce5611547e1a288b7d321
Gordon <gordonAdeacon@outlook.com>
2021-03-29 14:44:42 +0100

    Added more CSS changes to welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="min-h-screen bg-gray-100">
+    <div class="min-h-screen bg-gray-100 md:pb-10">
         <home-nav-layout :canLogin="canLogin" :canRegister="canRegister"/>
-        <div class="container mx-auto h-auto lg:w-3/5 mt-10 bg-white rounded-lg shadow-xl">
+        <div class="container mx-auto h-auto lg:w-3/5 md:mt-10 bg-white md:rounded-lg shadow-xl">
             <div class="flex justify-center">
                 <h2 class="text-2xl py-4 mt-5">Open Programs</h2>
             </div>
@@ -14,15 +14,15 @@
                             <p class="px-8 py-4 text-gray-500">{{program.Excerpt}}</p>
                         </div>
 
-                        <div class="px-1 flex flex-col justify-end lg:-my-px lg:mr-8 lg:ml-10 lg:flex-row lg:justify-items-center">
-                            <inertia-link v-if="$page.props.user && activeReportPresent(program.id)" :href="route('JoinProgram')" method="post" :data="{user_id: $page.props.user.id, program_id: program.id }" class="no-underline text-center inline-block text-sm px-4 py-2 my-1 leading-none border rounded hover:border-transparent text-white bg-green-400 hover:bg-green-300 lg:mt-4 sm:mt-0">
+                        <div class="px-1 flex flex-col md:flex-row md:items-center">
+                            <inertia-link v-if="$page.props.user && activeReportPresent(program.id)" :href="route('JoinProgram')" method="post" :data="{user_id: $page.props.user.id, program_id: program.id }" class="no-underline text-sm px-4 py-2 my-1 border rounded hover:border-transparent text-white bg-green-400 hover:bg-green-300 sm:mt-0">
                                 Join
                             </inertia-link>
-                            <inertia-link :href="route('home')" class="no-underline text-center inline-block text-sm px-4 py-2 my-1 lg:mr-2 lg:ml-2 leading-none border rounded hover:border-transparent text-white bg-blue-400 hover:bg-blue-300 lg:mt-4 sm:mt-0">
+                            <inertia-link :href="route('home')" class="no-underline text-center text-sm px-4 py-2 my-1 lg:mr-2 lg:ml-2 border rounded hover:border-transparent text-white bg-blue-400 hover:bg-blue-300 sm:mt-0">
                                 View
                             </inertia-link>
 
-                            <inertia-link :href="route('home')" class="no-underline text-center inline-block text-sm px-4 py-2 my-1 leading-none border rounded hover:border-transparent text-white bg-blue-400 hover:bg-blue-300 lg:mt-4 sm:mt-0">
+                            <inertia-link :href="route('home')" class="no-underline text-center text-sm px-4 py-2 my-1 border rounded hover:border-transparent text-white bg-blue-400 hover:bg-blue-300 sm:mt-0">
                                 Contact
                             </inertia-link>
                         </div>
