27496ab682499a9b2e6f9f7c2edfc7217ac9b4f6
Gordon <gordonAdeacon@outlook.com>
2021-03-29 10:58:44 +0100

    Added further changes to the CSS of Welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -8,11 +8,13 @@
 
             <div class="container mx-auto h-auto w-4/5 mt-5 pb-8 bg-white rounded-lg">
                 <div :key="program.id" v-for="program in programs" class="border rounded py-2 my-2">
-                    <div class="relative flex flex-col justify-between lg:flex-row lg:items-center">
-                        <h4 class="text-lg px-4 py-2">{{program.Title}}</h4>
-                        <p class="px-4 py-4">{{program.Excerpt}}</p>
-                        
-                        <div class="px-1 flex flex-col justify-end lg:-my-px lg:mr-8 lg:ml-10 lg:flex-row lg:justify-center">
+                    <div class="flex flex-col justify-center md:flex-row md:justify-between">
+                        <div class="flex flex-col md:flex-row md:justify-between">
+                            <h4 class="text-lg px-4 py-2 text-center">{{program.Title}}</h4>
+                            <p class="px-8 py-4 text-gray-500">{{program.Excerpt}}</p>
+                        </div>
+
+                        <div class="px-1 flex flex-col justify-end lg:-my-px lg:mr-8 lg:ml-10 lg:flex-row lg:justify-items-center">
                             <inertia-link v-if="$page.props.user && activeReportPresent(program.id)" :href="route('JoinProgram')" method="post" :data="{user_id: $page.props.user.id, program_id: program.id }" class="no-underline text-center inline-block text-sm px-4 py-2 my-1 leading-none border rounded hover:border-transparent text-white bg-green-400 hover:bg-green-300 lg:mt-4 sm:mt-0">
                                 Join
                             </inertia-link>
