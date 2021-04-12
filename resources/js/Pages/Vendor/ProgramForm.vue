06b2b6d2ed564240de6ae77d9cc6406051f0e89c
JSween <61639420+JSween-Code@users.noreply.github.com>
2021-04-12 02:11:55 +0100

    Add files via upload

---
+++ resources/js/Pages/Vendor/ProgramForm.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+                <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+
+                    <div class="col-span-6 sm:col-span-4">
+
+                        <h1 class="text-lg font-mediumtext-gray-900">
+                            Your Programs
+                        </h1>
+                    </div>
+
+                    <div>
+                        <div v-if="programs.length == 0" class="mt-10 text-sm text-gray-600">
+                            It looks like you don't have any programs. Why not create one now!
+                        </div>
+                        <div v-else-if="programs">
+                            <div v-for="program in programs">
+                                <jet-section-border />
+                                <div class="flex justify-between">
+                                    <div class="inline-block mt-4 font-bold">
+                                        {{program.Title}}
+                                    </div>
+                                    <button class="inline-block ml-10 mt-4 w-auto p-1 border-2 rounded border-red-600 bg-red-600 text-white hover:bg-red-700" @click="remove(program)">Delete</button>
+                                </div>
+                                <div class="mt-5">
+
+                                    <div class="mt-10 w-1/2">
+                                        <jet-label for="title">Program Title<span class="text-red-600"> *</span></jet-label>
+                                        <jet-input id="title" type="text" class="mt-1 block w-full" v-model="program.Title">
+                                            {{program.Title}}
+                                        </jet-input>
+                                    </div>
+
+                                    <div class="mt-8 w-1/2">
+                                        <jet-label for="description">Description<span class="text-red-600"> *</span></jet-label>
+                                        <textarea class="w-full h-48" v-model="program.Description">
+                                            {{program.Description}}
+                                        </textarea>
+                                    </div>
+
+                                    <button @click="update(program)" class="focus:outline-none text-white py-1 px-2.5 rounded-md bg-gray-700 hover:bg-gray-900 hover:shadow-lg mt-6">Update</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import JetButton from '@/Jetstream/Button'
+    import JetInput from '@/Jetstream/Input'
+    import JetLabel from '@/Jetstream/Label'
+    import JetSectionBorder from '@/Jetstream/SectionBorder'
+
+
+    export default {
+
+        components: {
+            JetButton,
+            JetInput,
+            JetLabel,
+            JetSectionBorder,
+        },
+
+        data() {
+            return {
+                deleteForm: this.$inertia.form({
+                    id: '',
+
+                }),
+
+                updateForm: this.$inertia.form({
+                    id: '',
+                    title: '',
+                    descr: '',
+                }),
+            }
+        },
+
+        props: {
+
+            programs: Array,
+        },
+
+        methods: {
+
+            remove(program) {
+
+                this.deleteForm.id = program.id;
+                this.deleteForm.post('/program-delete');
+
+            },
+
+            update(program) {
+                this.updateForm.id = program.id;
+                this.updateForm.title = program.Title;
+                this.updateForm.descr = program.Description;
+                this.updateForm.post('program-update');
+            }
+
+        }
+
+    }
+</script>
---
+++ resources/js/Pages/Vendor/ReportsForm.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+                <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+
+                    <div class="col-span-6 sm:col-span-4">
+
+                        <h1 class="text-lg font-mediumtext-gray-900">
+                            Submitted Reports
+                        </h1>
+                    </div>
+
+                    <div>
+                        <div v-if="reports.length == 0" class="mt-10 text-sm text-gray-600">
+                            No reports have been submitted for any of your programs.
+                        </div>
+                        <div v-else-if="reports">
+
+                            <table class="min-w-full divide-y divide-gray-200">
+
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Report Title</th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date Submitted</th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+                                        <th scope="col" class="relative px-6 py-3">Actions</th>
+                                    </tr>
+                                </thead>
+
+
+
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    <tr v-for="report in reports" :key="report.id">
+                                        <td class="px-6 py-4 whitespace-nowrap">{{ report.title }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap">{{ report.created_at }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap">{{ report.status }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-center text-indigo-600 hover:text-indigo-900">
+                                            <button @click="showModal(report)" class="focus:outline-none text-white text-sm py-1 px-2.5 rounded-md bg-gray-700 hover:bg-gray-900 hover:shadow-lg">View Report</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+
+                            </table>
+
+                        </div>
+                    </div>
+
+                    <jet-dialog-modal :show="reportShowModal" @close="reportCloseModal = false">
+
+                        <template #content>
+                            <div class="mb-2 font-semibold">Procedure: </div>
+                            <ul v-for="step in this.currentReport.procedure" class="list-disc">
+                                <li>{{ step }}</li>
+                            </ul>
+                        </template>
+
+                        <template #footer>
+                            <button @click.native="closeModal" class="inline-block ml-2 mt-10 mb-2 w-auto p-1 border-2 rounded border-red-600 bg-red-600 text-white hover:bg-red-700" >
+                                close
+                            </button>
+                        </template>
+                    </jet-dialog-modal>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>import JetButton from '@/Jetstream/Button'
+    import JetInput from '@/Jetstream/Input'
+    import JetLabel from '@/Jetstream/Label'
+    import JetSectionBorder from '@/Jetstream/SectionBorder'
+    import JetDialogModal from '@/Jetstream/DialogModal'
+
+
+    export default {
+
+        components: {
+            JetButton,
+            JetInput,
+            JetLabel,
+            JetSectionBorder,
+            JetDialogModal,
+        },
+
+        data() {
+            return {
+                reportShowModal: false,
+                currentReport: Object,
+            }
+        },
+
+        props: {
+
+            reports: Array,
+        },
+
+        created() {
+            for (let i = 0; i < this.$props.reports.length; i++) {
+                this.$props.reports[i].procedure = JSON.parse(this.$props.reports[i].procedure);
+            }     
+        },
+
+        methods: {
+
+            closeModal() {
+
+                this.reportShowModal = false;
+            },
+
+            showModal(report) {
+                this.currentReport = report;
+                this.reportShowModal = true;
+            },
+
+        }
+
+    }</script>
