950026d4f5f84afd451d33312f2fe0a7212dbd82
DrMantisToboggan-MD <78599596+DrMantisToboggan-MD@users.noreply.github.com>
2021-04-12 16:15:32 +0100

    Add files via upload

--- app/Listeners/GiveReporterPointsListener.php
+++ app/Listeners/GiveReporterPointsListener.php
@@ -3,13 +3,14 @@
 namespace App\Listeners;
 
 use App\Events\VerificationBatchCompleted;
-use App\Model\User;
+use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
 class GiveReporterPointsListener 
 {
-
+    private $user_id;
+    private $user;
     /**
      * Handle the event.
      *
@@ -17,8 +18,8 @@ class GiveReporterPointsListener
      * @return void
      */
     public function handle(VerificationBatchCompleted $event)
-    {
-       $user_id = $event->verificationBatch->report->creator_id;
+    {  
+        $user_id = $event->verificationBatch->report->creator_id;
         $user = User::find($user_id);
         $user->addPoint($points = 1000);
     }
