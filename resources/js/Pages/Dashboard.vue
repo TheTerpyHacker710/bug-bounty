d118bea39f30a808105da98b6fd6648a62e18ecd
Gordon <gordonAdeacon@outlook.com>
2021-04-16 15:51:01 +0100

    made dashboard responsive

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,36 +1,36 @@
 <template>
-    <app-layout>
-        <template #header>
-                <div class = "flex justify-between">
-                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                        User Dashboard
-                    </h2>
-                    <h2 class="font-semibold text-gray-800 leading-tight">
-                        <b>Reputation: </b> {{userInfo.reputation}}
-                    </h2>
-                </div>
-        </template>
+        <app-layout>
+            <template #header>
+                    <div class = "flex justify-between">
+                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                            User Dashboard
+                        </h2>
+                        <h2 class="font-semibold text-gray-800 leading-tight">
+                            <b>Reputation: </b> {{userInfo.reputation}}
+                        </h2>
+                    </div>
+            </template>
 
-        <div class="py-6 px-6">
-            <div class="max-w-full mx-auto">
-                <div class="grid gap-4 grid-cols-1 md:grid-cols-8 h-full">
+            <div class="py-6 px-6">
+                <div class="max-w-full mx-auto">
+                    <div class="grid gap-4 grid-cols-1 lg:grid-cols-8">
 
-                    <span class="bg-white col-span-1 md:col-span-2 shadow-xl rounded-lg h-full">
-                         <stats :chart="chart" :leaderboard="leaderboard"/>
-                    </span>
+                        <span class="bg-white col-span-1 lg:col-span-2 shadow-xl rounded-lg h-full">
+                            <stats :chart="chart" :leaderboard="leaderboard"/>
+                        </span>
 
-                    <span class="bg-white col-span-1 md:col-span-4 shadow-xl rounded-lg h-full">
-                        <active-jobs :activeReports="activeReports" :activeVerifications="activeVerifications"/>
-                    </span>
+                        <span class="bg-white col-span-1 lg:col-span-4 shadow-xl rounded-lg h-full">
+                            <active-jobs :activeReports="activeReports" :activeVerifications="activeVerifications"/>
+                        </span>
 
-                    <span class="bg-white col-span-1 md:col-span-2 shadow-xl rounded-lg h-full">
-                        <awards :userInfo="userInfo" :userBadges="userBadges"/>
-                    </span>
+                        <span class="bg-white col-span-1 lg:col-span-2 shadow-xl rounded-lg">
+                            <awards :userInfo="userInfo" :userBadges="userBadges"/>
+                        </span>
 
+                    </div>
                 </div>
             </div>
-        </div>
-    </app-layout>
+        </app-layout>
 </template>
 
 
